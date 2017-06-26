e187ae74698efba51b63e874382d297b910fc836
madhavik12 <madhukarup@yahoo.co.in>
2017-06-26 23:31:31 +0100

    This is a sample project
    this project just displays some text onto console

---
+++ src/sampleproject/myfirstclass.java
@@ -0,0 +1,11 @@
+package sampleproject;
+
+public class myfirstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello java");
+System.out.println("Welcome");
+	}
+
+}
